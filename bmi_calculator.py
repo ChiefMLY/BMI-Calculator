a41c143623ce4bdc713d0fba21bb9d7033340f5e
Yusuph Mustapha ladi <96384759+ChiefMLY@users.noreply.github.com>
2022-09-29 11:45:24 +0100

    Add files via upload

---
+++ bmi_calculator.py
@@ -0,0 +1,13 @@
+#Body mass index (BMI) calculator
+#Prompts the user to enter their height(m) and weight(kg)
+height = input("enter your height in m: ")
+weight = input("enter your weight in kg: ")
+
+#Calculates the bmi using the standard bmi formula and prints the ans as an integar
+bmi = float(weight) / float(height) ** 2)
+
+#Converting the bmi to an integar
+bmi_int = int(bmi)
+
+#Print the bmi
+print(bmi)
